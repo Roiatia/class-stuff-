949afb4f0ebbb3b241f8bcced8bc6b525869dbc6
roiat <roiat@Roi>
2025-11-10 17:30:09 +0200

    created player class

--- HW_1/src/HW1/Player.java
+++ HW_1/src/HW1/Player.java
@@ -1,5 +1,25 @@
 package HW1;
 
-public class Player {
+public class Player extends Character {
 
+	private item[] inventory;
+	public static final int CAPACITY = 10;
+	
+	
+	public Player() {
+		super();
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
